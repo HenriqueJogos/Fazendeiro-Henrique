2203ed77b723e69719d333593802bd25c131e1f5
HenriqueJogos <20241ctb0180023@estudantes.ifpr.edu.br>
2026-03-10 17:08:34 -0300

    Versao1
    Teste

---
+++ ProjetoUnity/Assets/Scripts/PlayerNewController.cs
@@ -0,0 +1,79 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerNewController : MonoBehaviour
+{
+    public float speed = 20f;
+    public float xRange = 15f;
+    public GameObject projectilePrefab;
+    public InputActionAsset InputActions;
+    public InputAction moveAction;
+    public InputAction fireAction;
+    public InputAction pauseAction;
+    public InputAction despauseAction;
+    public GameObject menuPause;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // antigo input system float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = moveAction.ReadValue<Vector2>().x;
+        // movimenta o player para esquerda e direita a partir da entrada do usu�rio
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+        // mant�m o player dentro dos limites do jogo (eixo x)
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.y);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.y);
+        }
+        if(fireAction.WasPressedThisFrame())
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        } 
+        if(pauseAction.WasPressedThisFrame())
+        {
+            Pause();
+        }
+        if(despauseAction.WasPressedThisFrame())
+        {
+            Despause();
+        }
+    }
+
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
+    private void Pause()
+    {
+        OnDisable();
+        InputActions.FindActionMap("UI").Enable();
+        menuPause.gameObject.SetActive(true);
+
+    }
+    private void Despause()
+    {
+        InputActions.FindActionMap("UI").Disable();
+        OnEnable();
+        menuPause.gameObject.SetActive(false);
+
+    }
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        fireAction = InputSystem.actions.FindAction("Jump");
+        pauseAction = InputSystem.actions.FindAction("Pause");
+        despauseAction = InputSystem.actions.FindAction("Despause");
+
+    }
+}
+
